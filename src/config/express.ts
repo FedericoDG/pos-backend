101ac86935dc52821ce8cdad0a3b353718487636
Federico González <alambratore@hotmail.com>
2024-02-20 16:12:07 -0300

    app-use /api

--- src/config/express.ts
+++ src/config/express.ts
@@ -11,7 +11,7 @@ const createServer = (): express.Application => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
-  app.use('/', router);
+  app.use('/api', router);
 
   app.use((_req, _res, next) => {
     next(createError(404));
